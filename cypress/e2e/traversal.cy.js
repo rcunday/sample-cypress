7e8b9cf84f8b90a4d5e6f2305c3e4632c04d184d
rcunday <rwcunday@gmail.com>
2023-07-25 23:47:46 -0600

    Add Traversal test using Lodash example

---
+++ cypress/e2e/traversal.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress"/>
+
+context("Traversal", function () {
+  beforeEach("Navigate to Traversal Content", function () {
+    cy.visit("/commands/traversal")
+    cy.fixture("testdata.json").then(function (testdata) {
+      this.testdata = testdata
+    })
+  })
+
+  it("use lodash to convert and map list items and compare against test data", function()  {
+    const getListItems = ($el) => {
+        return Cypress._.map($el,"innerText")
+    }
+    cy.get(".traversal-list>li")
+    .then(getListItems)
+    .should("deep.equal", this.testdata.traversalDetails.cats)
+  })
+})
