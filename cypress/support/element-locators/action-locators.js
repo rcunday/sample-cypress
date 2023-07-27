738f584281be50b4c77b892d496cf3e1e8b6dffc
rcunday <rwcunday@gmail.com>
2023-07-27 09:11:19 -0600

    Change from double to single quote string standard

--- cypress/support/element-locators/action-locators.js
+++ cypress/support/element-locators/action-locators.js
@@ -1,11 +1,11 @@
 export class ActionLocators {
-    actionInputEmail() {
-        return cy.get(".action-email")
-    }
-    actionSelectMultiple() {
-        return cy.get(".action-select-multiple")
-    }
-    actionSelectSingle() {
-        return cy.get(".action-select")
-    }
+  actionInputEmail() {
+    return cy.get('.action-email')
+  }
+  actionSelectMultiple() {
+    return cy.get('.action-select-multiple')
+  }
+  actionSelectSingle() {
+    return cy.get('.action-select')
+  }
 }
